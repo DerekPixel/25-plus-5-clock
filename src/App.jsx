08ac66231eb772ec4773b4671264c38b02597ae6
Derek Price <derekp098@gmail.com>
2021-04-25 23:35:25 -0700

    added header and classes

--- src/App.jsx
+++ src/App.jsx
@@ -10,47 +10,69 @@ function App() {
 
   return (
     <div className="App">
-      <div className="controls">
-        <div>
-          <div id="break-label">
-            Break Length
-          </div>
-          <button 
-            id="break-increment"
-            onClick={() => handleIncrement(setBreakLength, breakLength)}
-          >up</button>
-          <div
-            id="break-length"
-          >{breakLength}</div>
-          <button 
-            id="break-decrement"
-            onClick={() => handleDecrement(setBreakLength, breakLength)}
-          >down</button>
+      <header>
+        <h1 className="title">25 + 5 Clock</h1>
+        <div className="creds-div">
+          <p className="cred">Made by Derek Price</p>
+          <a className="cred" href="https://github.com/DerekPixel/25-plus-5-clock">GitHub</a>
         </div>
-        <div>
-          <div id="session-label">
-            Session Length
-          </div>
-          <button 
-            id="session-increment"
-            onClick={() => handleIncrement(setSessionLength, sessionLength)}
-          >up</button>
+      </header>
+      <div className="container">
+        <div className="controls">
           <div
-            id="session-length"
-          >{sessionLength}</div>
-          <button 
-            id="session-decrement"
-            onClick={() => handleDecrement(setSessionLength, sessionLength)}
-          >down</button>
+            className='break'
+          >
+            <h2 id="break-label">
+              Break Length
+            </h2>
+            <div
+              className='break-btns'
+            >
+              <button
+                id="break-increment"
+                onClick={() => handleIncrement(setBreakLength, breakLength)}
+              >up</button>
+              <div
+                className='length'
+                id="break-length"
+              >{breakLength}</div>
+              <button
+                id="break-decrement"
+                onClick={() => handleDecrement(setBreakLength, breakLength)}
+              >down</button>
+            </div>
+          </div>
+          <div
+            className='session'
+          >
+            <h2 id="session-label">
+              Session Length
+            </h2>
+            <div
+              className='session-btns'
+            >
+              <button
+                id="session-increment"
+                onClick={() => handleIncrement(setSessionLength, sessionLength)}
+              >up</button>
+              <div
+                className='length'
+                id="session-length"
+              >{sessionLength}</div>
+              <button
+                id="session-decrement"
+                onClick={() => handleDecrement(setSessionLength, sessionLength)}
+              >down</button>
+            </div>
+          </div>
         </div>
+        <Timer
+          breakLength={breakLength}
+          sessionLength={sessionLength}
+          setBreakLength={(newLength) => setBreakLength(newLength)}
+          setSessionLength={(newLength) => setSessionLength(newLength)}
+        />
       </div>
-
-      <Timer 
-        breakLength={breakLength} 
-        sessionLength={sessionLength}
-        setBreakLength={(newLength) => setBreakLength(newLength)}
-        setSessionLength={(newLength) => setSessionLength(newLength)}
-      />
 
     </div>
   );
