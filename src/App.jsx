965a6ad91ffe620e92efd8123068ad40cab8334e
Derek Price <derekp098@gmail.com>
2021-04-26 20:42:42 -0700

    added iconify stuff and some classes

--- src/App.jsx
+++ src/App.jsx
@@ -29,17 +29,23 @@ function App() {
               className='break-btns'
             >
               <button
+                className='increment'
                 id="break-increment"
                 onClick={() => handleIncrement(setBreakLength, breakLength)}
-              >up</button>
+              >
+                <span className="iconify" data-icon="bi:caret-up-fill"></span>
+              </button>
               <div
                 className='length'
                 id="break-length"
-              >{breakLength}</div>
+              ><p>{breakLength}</p></div>
               <button
+                className='decrement'
                 id="break-decrement"
                 onClick={() => handleDecrement(setBreakLength, breakLength)}
-              >down</button>
+              >
+                <span className="iconify" data-icon="bi:caret-down-fill"></span>
+              </button>
             </div>
           </div>
           <div
@@ -52,17 +58,23 @@ function App() {
               className='session-btns'
             >
               <button
+                className='increment'
                 id="session-increment"
                 onClick={() => handleIncrement(setSessionLength, sessionLength)}
-              >up</button>
+              >
+                <span className="iconify" data-icon="bi:caret-up-fill"></span>
+              </button>
               <div
                 className='length'
                 id="session-length"
-              >{sessionLength}</div>
+              ><p>{sessionLength}</p></div>
               <button
+                className='decrement'
                 id="session-decrement"
                 onClick={() => handleDecrement(setSessionLength, sessionLength)}
-              >down</button>
+              >
+                <span className="iconify" data-icon="bi:caret-down-fill"></span>
+              </button>
             </div>
           </div>
         </div>
